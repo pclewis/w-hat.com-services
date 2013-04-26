425ef51bd9426057f647cc756cd8f6a9f7f32739
Philip Lewis <pcl@pclewis.com>
2013-04-26 19:57:30 -0400

    first pass

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject name2key "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [http-kit "2.0.1"]
+                 [com.taoensso/carmine "1.7.0"]
+                 [ring/ring-core "1.1.8"]
+                 [ring/ring-devel "1.1.8"]]
+  :main name2key.core)
---
+++ test/name2key/test/core.clj
@@ -0,0 +1,6 @@
+(ns name2key.test.core
+  (:use [name2key.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
